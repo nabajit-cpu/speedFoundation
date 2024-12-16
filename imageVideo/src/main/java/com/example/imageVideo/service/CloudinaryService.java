0427a0bf4b96b12698db0a3598a78f56a082b6d1
nabajit-cpu <nabajitboro796@gmail.com>
2024-12-16 22:59:04 +0530

    imageVideo

---
+++ imageVideo/src/main/java/com/example/imageVideo/service/CloudinaryService.java
@@ -0,0 +1,28 @@
+package com.example.imageVideo.service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+
+@Service
+public class  CloudinaryService{
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public Map upload(MultipartFile file) throws IOException {
+        return cloudinary.uploader().upload(file.getBytes(),
+            ObjectUtils.asMap("resource_type", "auto"));
+    }
+
+    public Map delete(String publicId) throws IOException {
+        return cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+}
